a846f6570d4ae5d6296f0bd6597e263c0bba7999
daunniy <d_a_u_n@naver.com>
2024-10-10 14:09:09 +0900

    scss기본설정완료

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,23 @@ import React from 'react';
 
 const Header=()=>{
   return(
-    <div>Header</div>
+    <header id="header">
+      <div className="header_inner">
+        <h1 className="logo">Site portfolio</h1>
+        <nav className="header_nav" role="navigation" aria-label='메인메뉴'>
+          <ul>
+            <li><a href="#intro">Intro</a></li>
+            <li><a href="#ability">Ability</a></li>
+            <li><a href="#stie">Site</a></li>
+            <li><a href="#project">Project</a></li>
+            <li><a href="#contact">Contact</a></li>
+          </ul>
+        </nav>
+        <div className="header_nav_mobile" id="headerToggle" aria-controls="primary-menu" aria-expanded="false">
+          <span>btn</span>
+        </div>
+      </div>
+    </header>
   )
 }
 
